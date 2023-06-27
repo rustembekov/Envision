da295cc730fa99ddc3666f7f9c36ec36a57d98a0
Sabyrzhan <sayatbek@snoonu.com>
2023-06-27 19:18:05 +0600

    changed

--- components/serializers.py
+++ components/serializers.py
@@ -8,3 +8,5 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ["username", "email", "password"]
 
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
--- components/views.py
+++ components/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from .permissions import IsAdminReadOnly
-from .serializer import UserSerializer
+from .serializers import UserSerializer
 
 
 class UserViewSetList(mixins.ListModelMixin,
